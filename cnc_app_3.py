55606e836827438d86bf6d7f0959979f0a122e6c
rgloverhh <156850648+rgloverhh@users.noreply.github.com>
2024-06-18 15:34:50 -0700

    Add files via upload

--- cnc_app_3.py
+++ cnc_app_3.py
@@ -21,10 +21,10 @@ def prediction(input1, input2, input3, input4):
 
 st.text("Please fill in the responses below to predict primary care service level")
 
-calls_offered = st.number_input(label="Enter a call volume between 500 and 3000", min_value=500, max_value=4000, step=10)
-aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.1 = 6 sec)", min_value=4.0, max_value=7.0, step=0.1)
-not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=35.0, step=0.5)
-ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=20.0, max_value=40.0, step=0.5)
+calls_offered = st.number_input(label="Enter a call volume between 500 and 3000", min_value=500, max_value=4000, step=10, value="")
+aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.1 = 6 sec)", min_value=4.0, max_value=7.0, step=0.1, value="")
+not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=35.0, step=0.5, value="")
+ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=20.0, max_value=40.0, step=0.5, value="")
 
 not_ready_con = not_ready/100
 
@@ -32,7 +32,9 @@ sl_prediction_temp = prediction(calls_offered, aht, not_ready_con, ftes_logged_i
 sl_prediction = round((sl_prediction_temp*100),1)
 
 st.header("Service Level Prediction")
-if sl_prediction <= 0:
+if sl_prediction == "":
+    st.subheader("Load predictors")
+elif sl_prediction <= 0:
     st.subheader("0%")
 elif sl_prediction >= 100:
     st.subheader("100%")
