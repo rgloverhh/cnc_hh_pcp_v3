353e7611fea2fc72ac6dc48c38a5739ee7d98898
rgloverhh <156850648+rgloverhh@users.noreply.github.com>
2024-06-18 15:56:07 -0700

    Add files via upload

--- cnc_app_3.py
+++ cnc_app_3.py
@@ -19,8 +19,8 @@ def prediction(input1, input2, input3, input4):
 
 #inputs - calls offered, AHT, not ready rate, total ftes, fte callouts
 
-st.text("Please fill in the responses below to predict primary care service level")
-st.text("Default values are daily average from May 2024")
+st.text("**Please fill in the responses below to predict primary care service level**")
+st.markdown("*Default values are daily average from May 2024*")
 
 calls_offered = st.number_input(label="Enter a call volume between 500 and 3000", min_value=500, max_value=4000, step=10, value=1970)
 aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.1 = 6 sec)", min_value=4.0, max_value=7.0, step=0.05, value=5.50)
